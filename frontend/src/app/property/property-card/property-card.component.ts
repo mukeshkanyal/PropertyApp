2213831e435fb43aada18b35ef10def78763350c
Mukesh Singh Kanyal <kanyalbca.93@gmail.com>
2023-07-02 11:30:29 +0530

    Initial project

---
+++ frontend/src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-property-card',
+  templateUrl: './property-card.component.html',
+  styleUrls: ['./property-card.component.css']
+})
+export class PropertyCardComponent {
+property:any = {
+  "Id":1,
+  "Type":"House",
+  "Price":1200,
+  "Name":"Birla House"
+}
+}
